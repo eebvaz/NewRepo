5e5a92292eced109984003d6ca236c3443f7aeac
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-05-13 20:48:27 -0600

    En proceso

--- Program.cs
+++ Program.cs
@@ -1,40 +1,185 @@
-﻿using System.Security.AccessControl;
+﻿
 
-class uno
+
+class Cliente
+{
+    private string estado; 
+    private double valor;
+    private string encargo;
+
+    public Cliente(string estado, double valor, string encargo)
+    {
+        this.estado = estado;
+        this.valor = valor;
+        this.encargo = encargo;
+    }
+
+    public string Estado
+    {
+        get { return estado; }
+        set { estado = value; }
+    }
+    public double Valor
+    {
+        get { return valor; }
+        set { valor = value; }
+    }
+    public string Encargo
+    {
+        get { return encargo; }
+        set { encargo = value; }
+    }
+}
+
+//////////////////////////////////////////////////
+
+class Persona : Cliente
+{
+    private string comprador;
+    private string telefono;
+
+
+    public Persona (string estado, double valor, string encargo,string comprador, string telefono): 
+        base (estado, valor, encargo)
+    {
+        this.comprador = comprador;
+        this.telefono = telefono;
+
+    }
+
+    public string Comprador
+    {
+        get { return comprador; }
+        set { comprador = value; }
+    }
+    public string Telefono
+    {
+        get { return telefono; }
+        set { telefono = value; }
+    }
+
+
+}
+
+//////////////////////////////////////////////////
+class Productos
 {
     private int codigo;
     private string nombre;
     private DateTime fecha;
     private double precio;
     private int cantidad;
+    private int vendidos;
 
-    private string estado; 
-    private double valor;
-    private string encargo;
+    public Productos(int codigo, string nombre, DateTime fecha, double precio, int cantidad, int vendidos)
+    {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.vendidos = vendidos;
+    }
+
+    public int Codigo
+    {
+        get { return codigo; }
+      set { codigo = value; }
+
+    }
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+    public DateTime Fecha
+    {
+        get { return fecha; }
+        set { fecha = value; }
+    }
+    public double Precio
+    {
+        get{ return precio; }
+        set { precio = value; }
+    }
+    public int Cantidad
+    {
+        get{ return cantidad; }
+        set { cantidad = value; }
+    }
+    public int Vendidos
+    {
+         get { return vendidos; }
+        set { vendidos = value; }
+    }
 
 
+    public double Total()
+    {
+        return precio * cantidad;
+    }
+
+    public string EstadoProducto()
+    {
+        if(cantidad <= 0)
+        {
+            return "No disponible";
+        }
+        else if(fecha < DateTime.Now)
+        {
+            return "Vencido";
+        }
+        else if((fecha - DateTime.Now).Days <= 0)
+        {
+            return "Por vencer";
+        }    
+        else
+        {
+            return "Disponible";
+        }
+    }
+
+
+    public string ObtenerDatos()
+    {
+        return "| Codigo: " + codigo + Environment.NewLine
+            + "| Nombre: " + nombre + Environment.NewLine
+            + "| Fecha: " + fecha.ToShortDateString() + Environment.NewLine
+            + "| Precio: " + precio + Environment.NewLine
+            + "| Cantidad: " + cantidad + Environment.NewLine
+            + "| Vendidos: " + vendidos + Environment.NewLine
+            + "| Estado: " + EstadoProducto() + Environment.NewLine;
+    }
+
+    public void GuardarArchivo(string ruta)
+    {
+        File.AppendAllText(ruta, ObtenerDatos()+Environment.NewLine);
+    }
 
 }
 
-class dos : uno
-{
 
-}
+//////////////////////////////////////////////////
 
-class tres : uno
-{
 
-}
 
 class Programa
 {
     static void Main()
     { 
 
-       Dictionary <int, dos> dato = new Dictionary <int, dos> ();
-         
+      
+       Dictionary <int, Productos> products = new Dictionary<int, Productos> ();
 
-        int opcion;
+
+        string ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"Registro.txt");
+
+        int codigo, cantidad, vendidos;
+        string nombre;
+        DateTime fecha;
+         double precio;
+        bool validacion;
+       int opcion;
 
         do {
 
@@ -55,18 +200,65 @@ class Programa
          switch(opcion)
             {
                     case 1:
-                    Console.WriteLine("");
+                    Console.WriteLine("------------Vender Producto-----------");
 
+                    int codigoBarra;
+                    do
+                    {
+                        Console.WriteLine("Ingrese el codigo");
+                        validacion = int.TryParse(Console.ReadLine(), out codigoBarra);
+                        if(!validacion || codigoBarra <= 0)
+                        {
+                            Console.WriteLine("Codigo invalido");
+                            validacion = false;
+                        }
+
+                    } while (!validacion);
+
+
+                    if(products.ContainsKey(codigoBarra))
+                    {
+                        int vender;
+                        do
+                        {
+                            Console.WriteLine("Ingrese el codigo");
+                            validacion = int.TryParse(Console.ReadLine(), out vender);
+                            if (!validacion || vender <= 0)
+                            {
+                                Console.WriteLine("Cantidad invalida");
+                                validacion = false;
+                            }
+                        } while (!validacion);
+                    }
+
+
+
+                    ///////////////////////////////////////
                     break;
                     case 2:
-                    Console.WriteLine("");
+                    Console.WriteLine("---------Agregar Producto-------");
+
+                    do
+                    {
+                        Console.Write("Ingrese fecha nacimiento: ");
+                        validacion = DateTime.TryParse(Console.ReadLine(), out fecha);
+
+                        if (!validacion)
+                        {
+                            Console.WriteLine("Fecha invalida");
+                        }
+                    } while (!validacion);
 
 
+                   
+
+                    ///////////////////////////////////////
                     break;
                     case 3:
                     Console.WriteLine("");
 
 
+                    ///////////////////////////////////////
                     break;
                     case 4:
                     Console.WriteLine("");
